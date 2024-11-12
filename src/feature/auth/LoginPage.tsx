cada3a35c29c69cf83e32e5c2e166bd56bdc81d3
olehsokaslkyi <oleh.sokalskyi@oa.edu.ua>
2024-11-13 00:25:19 +0200

    Add FormInput component

--- src/feature/auth/LoginPage.tsx
+++ src/feature/auth/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import FormInput from '../../component/FormInput'
 import LoginService from './service/auth.service'
 
 const loginService = new LoginService()
@@ -29,35 +30,38 @@ const LoginPage = () => {
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Your email
               </label>
-              <input type="email" name="email" id="email"
-                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg
-                      focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
-                      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                      dark:focus:border-blue-500"
-                     placeholder="name@company.com" required  {...register('email', {
-                required: 'Email is required',
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                  message: 'Invalid email address'
-                }
-              })} />
-              {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
+             <FormInput
+               name={"email"}
+               type={"email"}
+               placeholder={"test@example.com"}
+               options={{
+                 required: 'Email is required',
+                 pattern: {
+                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                   message: 'Invalid email address'
+                 }
+               }}
+               register={register}
+               errors={errors}
+               />
             </div>
             <div>
               <label htmlFor="password"
                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••"
-                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600
-                      focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                       dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                     required=""
-                     {...register('password', {
-                       required: 'Password is required',
-                       minLength: { value: 8, message: 'Password must be at least 8 characters' },
-                       maxLength: { value: 16, message: 'Password must be at most 16 characters' }
-                     })}
-              />
-              {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password.message}</p>}
+                <FormInput
+                    name={"password"}
+                    type={"password"}
+                    placeholder={"Your password"}
+                    options={{
+                        required: 'Password is required',
+                        minLength: {
+                        value: 6,
+                        message: 'Password must have at least 6 characters'
+                        }
+                    }}
+                    register={register}
+                    errors={errors}
+                />
             </div>
             <button type="submit"
                     className="w-full text-white bg-primary-600 hover:bg-primary-700
