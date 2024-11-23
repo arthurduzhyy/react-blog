d72de01e760894e2b7a106f0b2343262bf91ca14
olehsokaslkyi <oleh.sokalskyi@oa.edu.ua>
2024-11-23 17:47:19 +0200

    Replace theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from './context/ThemeContext'
+import { ThemeProvider } from './feature/theme/context/ThemeContext'
 import BasicDataRouter from './route/BasicDataRouter'
 
 function App() {
--- src/feature/theme/ThemeToggleButton.tsx
+++ src/feature/theme/ThemeToggleButton.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from '../../context/ThemeContext'
+import { useTheme } from './context/ThemeContext'
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'
 
 const ThemeToggleButton = () => {
